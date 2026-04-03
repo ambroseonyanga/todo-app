17c4b3e26ed160010fc0ff4c08edc0d006bd0643
ambrose <amb.onyanga@gmail.com>
2026-04-03 13:09:41 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env file
+
+app = Flask(__name__)
+
+# Database connection using environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///todos.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# --- Database Model ---
+class Todo(db.Model):
+    id  = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    tag  = db.Column(db.String(50), default='personal')
+
+    def __repr__(self):
+        return f'<Todo {self.task}>'
+
+
+# --- Routes ---
+
+# READ - Show all tasks
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+
+# CREATE - Add a new task
+@app.route('/add', methods=['POST'])
+def add():
+    task_text = request.form.get('task')
+    tag       = request.form.get('tag', 'personal')
+    if task_text:
+        new_todo = Todo(task=task_text, tag=tag)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+
+# UPDATE - Mark task as done / undone
+@app.route('/toggle/<int:id>', methods=['POST'])
+def toggle(id):
+    todo = Todo.query.get_or_404(id)
+    todo.done = not todo.done
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+# DELETE - Remove a task
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Creates tables if they don't exist
+    app.run(debug=True)
